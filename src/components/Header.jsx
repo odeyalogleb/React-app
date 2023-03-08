2154d7c67b0eb9d6ad7506c5b5cdb695663bce6a
Bbar0n234 <vilnus1337@mail.ru>
2023-03-08 22:57:46 +0300

    third commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://i.pinimg.com/originals/0f/41/d0/0f41d0404ae75869088e7c150affab84.png'/>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+            <img src='https://storge.pic2.me/c/1360x800/623/55661684bf63f.jpg'/>
+            </div>
+            <div>
+            ava + info
+            </div>
+            <div>
+            My posts
+            <div>New post</div>
+            <div>
+                <div>post 1</div>
+                <div>post 2</div>
+            </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
