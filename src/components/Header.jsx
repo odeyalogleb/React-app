f3545fd95d2a338ab26855b7410fee84095b524b
Bbar0n234 <vilnus1337@mail.ru>
2023-03-09 20:09:30 +0300

    after 14 lesson

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
+import classes from './Header.module.css';
+
 const Header = () => {
     return (
-        <header className='header'>
+        <header className={classes.header}>
             <img src='https://i.pinimg.com/originals/0f/41/d0/0f41d0404ae75869088e7c150affab84.png'/>
         </header>
     );
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,6 +1,10 @@
+import classes from './Profile.module.css';
+
+
+
 const Profile = () => {
     return (
-        <div className='content'>
+        <div className={classes.content}>
             <div>
             <img src='https://storge.pic2.me/c/1360x800/623/55661684bf63f.jpg'/>
             </div>
@@ -11,8 +15,8 @@ const Profile = () => {
             My posts
             <div>New post</div>
             <div>
-                <div>post 1</div>
-                <div>post 2</div>
+                <div className={classes.item}>post 1</div>
+                <div className={classes.item}>post 2</div>
             </div>
             </div>
         </div>
