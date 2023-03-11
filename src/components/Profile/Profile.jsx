fbdc6fedaced6ad490aec187a9f4a7552f2c7024
Bbar0n234 <vilnus1337@mail.ru>
2023-03-11 12:39:14 +0300

    after 18 lesson

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -5,9 +5,9 @@ import MyPosts from './MyPosts/MyPosts';
 
 const Profile = () => {
     return (
-        <div className={classes.content}>
-            <div className={classes.content_header}>
-                <img className={classes.content__img}src='https://storge.pic2.me/c/1360x800/623/55661684bf63f.jpg'/>
+        <div>
+            <div className={classes.header}>
+                <img src='https://storge.pic2.me/c/1360x800/623/55661684bf63f.jpg' alt=''/>
             </div>
             <div>
             ava + info
