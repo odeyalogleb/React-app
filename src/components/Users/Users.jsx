46d29743b88674edb8fa56cb8955b870d7387e7a
Bbar0n234 <vilnus1337@mail.ru>
2023-03-26 20:44:20 +0300

    after 53 lesson

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,45 +1,25 @@
 import axios from "axios";
 import User from "./User/User"
+import React from "react";
 
-const Users = (props) => {
-    
-    
-    if (props.users.length === 0) {
+class Users extends React.Component {
+    constructor(props){
+        super(props);
+
         axios.get("https://social-network.samuraijs.com/api/1.0/users")
-        .then(response => {
-            props.setUsers(response.data.items);
+            .then(response => {
+                props.setUsers(response.data.items);
         });
-        {/*props.setUsers( 
-            [{
-                id: 1, followed: true,
-                photoURL: "https://i.pinimg.com/originals/85/79/78/857978afa22ddb0bf7c4a6a68b7afb05.jpg",
-                location: { country: "Russia", city: "Moscow" }, status: "i love rus", name: "STAS S"
-            },
-            {
-                id: 2, followed: false,
-                photoURL: "https://i.pinimg.com/originals/85/79/78/857978afa22ddb0bf7c4a6a68b7afb05.jpg",
-                location: { country: "Russia", city: "SPB" }, status: "i am boss", name: "STAS A"
-            },
-            {
-                id: 3, followed: true,
-                photoURL: "https://i.pinimg.com/originals/85/79/78/857978afa22ddb0bf7c4a6a68b7afb05.jpg",
-                location: { country: "Russia", city: "TLT" }, status: "i am stas", name: "STAS F"
-            }]
-        );*/}
-
     }
-    let userElements = props.users.map(u => <User data={u} follow={props.follow} unfollow={props.unfollow} />);
 
-    
-
-    
-    return (
-        <div>
-            {userElements}
-        </div>
-
-    );
+    render(){
+        let userElements = this.props.users.map(u => <User data={u} follow={this.props.follow} unfollow={this.props.unfollow} />);
+        return (
+            <div>
+                {userElements}
+            </div>
+        );
+    }
 }
 
 export default Users;
-
