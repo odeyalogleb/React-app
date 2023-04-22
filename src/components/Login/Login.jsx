adffd5e6dda3d3b159a0534358ef1447b59d7939
Bbar0n234 <vilnus1337@mail.ru>
2023-04-22 20:54:08 +0300

    after 75 lesson

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,38 @@
-const Login = (props) => {
+import { Field, reduxForm } from "redux-form"
+
+const Login = () => {
+    const onSubmit = (formData) => {
+        console.log(formData)
+    }
     return (
-        <h1> LOGIN</h1>
+        <div>
+            <h1>LOGIN</h1>
+            <LoginReduxForm onSubmit={onSubmit} />
+        </div>
     )
 }
+
+const LoginForm = (props) => {
+    return (
+        <form onSubmit={props.handleSubmit} >
+            <div>
+                <Field component={"input"} name={"login"} placeholder={"Login"} />
+            </div>
+            <div>
+                <Field component={"input"} name={"password"} placeholder={"password"} />
+            </div>
+            <div>
+                <Field component={"input"}  type={"checkbox"} name={"remeberMe"} /> remember me
+            </div>
+            <div>
+                <button type="submit">Login</button>
+            </div>
+        </form>
+    )
+}
+
+const LoginReduxForm = reduxForm({ form: 'login' })(LoginForm)
+
+
 
 export default Login;
--- src/redux/redux-state.js
+++ src/redux/redux-state.js
@@ -5,6 +5,7 @@ import navbarReducer from "./navbarReducer";
 import profileReducer from "./profileReducer";
 import usersReducer from "./usersReducer";
 import thunkMiddleware from 'redux-thunk'
+import { reducer as formReducer } from "redux-form";
 
 let reducers = combineReducers({
     profilePage: profileReducer,
@@ -12,6 +13,7 @@ let reducers = combineReducers({
     navbar: navbarReducer,
     usersPage: usersReducer,
     auth: authReducer,
+    form: formReducer
 })
 
 let store = legacy_createStore(reducers, applyMiddleware(thunkMiddleware));
