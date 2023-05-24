e288c5300323618937754f6e477ba67cf6387207
Bbar0n234 <vilnus1337@mail.ru>
2023-05-24 18:54:59 +0300

    after 85 lesson

--- src/components/Profile/ProfileHeader/ProfileStatusWithHooks.jsx
+++ src/components/Profile/ProfileHeader/ProfileStatusWithHooks.jsx
@@ -1,10 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const ProfileStatus = (props) => {
 
     let [editMode, setEditMode] = useState(false);
-
     let [status, setStatus] = useState(props.status);
+
+    useEffect(() => {
+        setStatus(props.status);
+    }, [props.status]);
 
     let onStatusChange = (e) => {
         setStatus(e.currentTarget.value);
